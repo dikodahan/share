9045e6569f611020174f233a22e1f951155a809e
Moshe Dahan <38052895+dikodahan@users.noreply.github.com>
2023-01-20 15:38:08 -0500

    Initial Crystal generator file

---
+++ src/services/crystal/crystal.generator.ts
@@ -0,0 +1,22 @@
+import Crystal from "./crystal.json";
+import { UserException } from "../../user-exception";
+import { epgGenerator } from "../epg.generator";
+
+export function* CrystalGenerator(
+  username: string,
+  password: string
+): Generator<string, void, unknown> {
+  if (!username || !password) {
+    throw new UserException("Invalid username or password", 400);
+  }
+
+  for (const line of epgGenerator()) {
+    yield line;
+  }
+
+  for (const { tvgId, tvgLogo, groupTitle, channelName, channelId } of LiveGo) {
+    yield "";
+    yield `#EXTINF:-1 tvg-id="${tvgId}" tvg-logo="${tvgLogo}" group-title="${groupTitle}",${channelName}`;
+    yield `http://crystal.ottc.pro:80/${username}/${password}/${channelId}`;
+  }
+}
