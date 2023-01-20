1c01a38d4a08b15f05a6f070465d0549142c6eaa
Moshe Dahan <38052895+dikodahan@users.noreply.github.com>
2023-01-20 15:43:39 -0500

    Added Crystal service

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,10 +1,12 @@
 // @spellchecker: ignore livego
 import { antiFrizGenerator } from "./antifriz/antifriz.generator";
 import { liveGoGenerator } from "./livego/livego.generator";
+import { CrystalGenerator } from "./livego/crystal.generator";
 
 type M3uGenerator = (username: string, password: string) => Generator<string, void, unknown>;
 
 export const SERVICE_GENERATORS: Record<string, M3uGenerator> = {
     "livego": liveGoGenerator,
+    "crystal": CrystalGenerator,
     "antifriz": antiFrizGenerator
 }
