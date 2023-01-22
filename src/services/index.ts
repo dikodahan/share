ba01f6104c019eb67d1ca6424c84f8fdf24d402d
Moshe Dahan <38052895+dikodahan@users.noreply.github.com>
2023-01-21 22:42:13 -0500

    Added testing

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,11 +2,13 @@
 import { antiFrizGenerator } from "./antifriz/antifriz.generator";
 import { liveGoGenerator } from "./livego/livego.generator";
 import { crystalGenerator } from "./crystal/crystal.generator";
+import { liveGoTestGenerator } from "./testing/livegotest.generator";
 
 type M3uGenerator = (username: string, password: string) => Generator<string, void, unknown>;
 
 export const SERVICE_GENERATORS: Record<string, M3uGenerator> = {
     "livego": liveGoGenerator,
     "crystal": crystalGenerator,
-    "antifriz": antiFrizGenerator
+    "antifriz": antiFrizGenerator,
+    "testing": liveGoTestGenerator
 }
---
+++ src/services/testing/livegotest.generator.ts
@@ -0,0 +1,25 @@
+import LiveGoTest from "./livegotest.json";
+import { UserException } from "../../user-exception";
+import { epgGenerator } from "../epg.generator";
+
+export function* liveGoTestGenerator(
+  username: string,
+  password: string
+): Generator<string, void, unknown> {
+  if (!username || !password) {
+    throw new UserException("Invalid username or password", 400);
+  }
+  else if (username == "USERNAME" || password == "PASSWORD") {
+    throw new UserException("Invalid username or password", 400);
+  }
+
+  for (const line of epgGenerator()) {
+    yield line;
+  }
+
+  for (const { tvgId, tvgLogo, groupTitle, channelName, channelId } of LiveGoTest) {
+    yield "";
+    yield `#EXTINF:-1 tvg-id="${tvgId}" tvg-logo="${tvgLogo}" group-title="${groupTitle}",${channelName}`;
+    yield `http://livego.club:8080/${username}/${password}/${channelId}`;
+  }
+}
