2c0596b3a281bce1987056cf0a5c53386630c53f
Moshe Dahan <38052895+dikodahan@users.noreply.github.com>
2023-01-22 21:15:46 -0500

    Added LiveGo Dark Mode

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,13 +1,16 @@
 // @spellchecker: ignore livego
 import { antiFrizGenerator } from "./antifriz/antifriz.generator";
 import { liveGoGenerator } from "./livego/livego.generator";
+import { liveGoDmGenerator } from "./livegodm/livegodm.generator";
 import { crystalGenerator } from "./crystal/crystal.generator";
 import { liveGoTestGenerator } from "./livegotest/livegotest.generator";
+
 
 type M3uGenerator = (username: string, password: string) => Generator<string, void, unknown>;
 
 export const SERVICE_GENERATORS: Record<string, M3uGenerator> = {
     "livego": liveGoGenerator,
+    "livegodm": liveGoDmGenerator,
     "crystal": crystalGenerator,
     "antifriz": antiFrizGenerator,
     "livegotest": liveGoTestGenerator
---
+++ src/services/livegodm/livegodm.generator.ts
@@ -0,0 +1,22 @@
+import LiveGoDm from "./livegodm.json";
+import { UserException } from "../../user-exception";
+import { epgGenerator } from "../epg.generator";
+
+export function* liveGoDmGenerator(
+  username: string,
+  password: string
+): Generator<string, void, unknown> {
+  if (!username || !password || username == "USERNAME" || password == "PASSWORD") {
+    throw new UserException("Invalid username or password", 400);
+  }
+
+  for (const line of epgGenerator()) {
+    yield line;
+  }
+
+  for (const { tvgId, tvgLogo, groupTitle, channelName, channelId } of LiveGoDm) {
+    yield "";
+    yield `#EXTINF:-1 tvg-id="${tvgId}" tvg-logo="${tvgLogo}" group-title="${groupTitle}",${channelName}`;
+    yield `http://livego.club:8080/${username}/${password}/${channelId}`;
+  }
+}
