d7da925ccfd191a4027be16a3f11a6ac00b39322
Moshe Dahan <38052895+dikodahan@users.noreply.github.com>
2023-05-20 10:52:29 -0400

    Fixing LiveGo

--- src/services/epg.generator.ts
+++ src/services/epg.generator.ts
@@ -1,11 +1,11 @@
 import EPG from "./epg.json";
 
 export function* epgGenerator(): Generator<string, void, unknown> {
-  yield `#EXTM3U url-tvg="${EPG.preferred}"`;
+  yield `#EXTM3U url-tvg="${EPG.dikoplus}"`;
   yield "";
 }
 
 export function* epgGeneratorUs(): Generator<string, void, unknown> {
-  yield `#EXTM3U url-tvg="${EPG.secondary}"`;
+  yield `#EXTM3U url-tvg="${EPG.epgsahre}"`;
   yield "";
 }
