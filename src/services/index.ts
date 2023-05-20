01d0b0744a374e6896a9a479fe0d1f13846ab016
Moshe Dahan <38052895+dikodahan@users.noreply.github.com>
2023-05-20 11:20:56 -0400

    removing livego service

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,7 +2,6 @@ import { antiFrizGenerator } from "./antifriz/antifriz.generator";
 import { antiFrizDmGenerator } from "./antifrizdm/antifrizdm.generator";
 import { edemGenerator } from "./edem/edem.generator";
 import { edemDmGenerator } from "./edemdm/edemdm.generator";
-import { liveGoGenerator } from "./livego/livego.generator";
 import { liveGoUsGenerator } from "./livegous/livegous.generator";
 import { liveGoDmGenerator } from "./livegodm/livegodm.generator";
 import { crystalGenerator } from "./crystal/crystal.generator";
@@ -15,7 +14,6 @@ import { liveGoTestGenerator } from "./livegotest/livegotest.generator";
 type M3uGenerator = (username: string, password: string) => Generator<string, void, unknown>;
 
 export const SERVICE_GENERATORS: Record<string, M3uGenerator> = {
-    "livego": liveGoGenerator,
     "livegodm": liveGoDmGenerator,
     "livegous": liveGoUsGenerator,
     "crystal": crystalGenerator,
