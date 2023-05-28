a2092aea495a766f50811e77bd34bdec13eb59e5
Moshe Dahan <38052895+dikodahan@users.noreply.github.com>
2023-05-28 17:31:34 -0400

    Added Livego

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,8 +1,10 @@
+import { liveGoGenerator } from "./livego/livego.generator";
+import { liveGoDmGenerator } from "./livegodm/livegodm.generator";
+import { liveGoUsGenerator } from "./livegous/livegous.generator";
 import { antiFrizGenerator } from "./antifriz/antifriz.generator";
 import { antiFrizDmGenerator } from "./antifrizdm/antifrizdm.generator";
 import { edemGenerator } from "./edem/edem.generator";
 import { edemDmGenerator } from "./edemdm/edemdm.generator";
-import { liveGoDmGenerator } from "./livegodm/livegodm.generator";
 import { crystalGenerator } from "./crystal/crystal.generator";
 import { crystalDmGenerator } from "./crystaldm/crystaldm.generator";
 import { dinoGenerator } from "./dino/dino.generator";
@@ -13,6 +15,8 @@ import { liveGoTestGenerator } from "./livegotest/livegotest.generator";
 type M3uGenerator = (username: string, password: string) => Generator<string, void, unknown>;
 
 export const SERVICE_GENERATORS: Record<string, M3uGenerator> = {
+    "livego": liveGoGenerator,
+    "livegodm": liveGoDmGenerator,
     "livegous": liveGoUsGenerator,
     "crystal": crystalGenerator,
     "crystaldm": crystalDmGenerator,
