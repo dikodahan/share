00e406a7f04e1aa20631b8137850456b368d544f
Moshe Dahan <38052895+dikodahan@users.noreply.github.com>
2023-06-13 10:48:42 -0700

    removed channel lineup dependency

--- backend/src/services/livegotest/livegotest.generator.ts
+++ backend/src/services/livegotest/livegotest.generator.ts
@@ -1,35 +1,8 @@
 import LiveGoTest from "./livegotest.json";
 import { UserException } from "../../user-exception";
 import { epgGenerator } from "../epg.generator";
-import { ChannelLineup } from "../channel.lineup.json";
 
 export function* liveGoTestGenerator(
-  username: string,
-  password: string
-): Generator<string, void, unknown> {
-  if (!username || !password || username == "USERNAME" || password == "PASSWORD") {
-    throw new UserException("Invalid username or password", 400);
-  }
-
-  for (const line of epgGenerator()) {
-    yield line;
-  }
-
-  for (const { channelName, tvgName, channelId } of LiveGoTest) {
-    const channelData = ChannelLineup.find((channel: any) => channel.channelName === channelName);
-
-    if (channelData) {
-      yield "";
-      yield `#EXTINF:-1 tvg-id="${channelData.tvgId}" tvg-name=${tvgName} tvg-logo="${channelData.tvgLogo}",${channelName}`;
-      yield `#EXTGRP:${channelData.extGrp}`;
-      yield `http://livego.club:8080/${username}/${password}/${channelId}`;
-    }
-  }
-}
-
-
-
-/* export function* liveGoTestGenerator(
   username: string,
   password: string
 ): Generator<string, void, unknown> {
@@ -47,4 +20,4 @@ export function* liveGoTestGenerator(
     yield `#EXTGRP:${extGrp}`;
     yield `http://livego.club:8080/${username}/${password}/${channelId}`;
   }
-} */
+}
