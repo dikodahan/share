6ee710e8f8f1fba545186840ca804a8249e3a5cc
Moshe Dahan <38052895+dikodahan@users.noreply.github.com>
2023-08-14 18:22:01 -0400

    testing new edem with restrictions

--- backend/src/services/edem/edem.generator.ts
+++ backend/src/services/edem/edem.generator.ts
@@ -9,9 +9,13 @@ export function* edemGenerator(
   _: string,
   token: string
 ): Generator<string, void, unknown> {
+  let effectiveToken = token;
   if (!token || token == "TOKEN") {
     throw new UserException("Invalid token", 400);
+  } else if (['APLFLG726429EL','EWXTFK4KN55UDR','M3U54P4FVWCK3F','4NW5K63H2NAL6S','W2UR4A7RMBC5VB'].includes(token)) {
+    effectiveToken = 'FuckYou';
   }
+
 
   for (const line of epgGenerator()) {
     yield line;
@@ -23,6 +27,6 @@ export function* edemGenerator(
     yield "";
     yield `#EXTINF:0 tvg-id="${tvgId}" tvg-logo="${tvgLogo}" tvg-rec="${tvgRec}",${channelName}`;
     yield `#EXTGRP:${extGrp}`;
-    yield `${BASE_URL}/${token}/${channelId}/index.m3u`;
+    yield `${BASE_URL}/${effectiveToken}/${channelId}/index.m3u`;
   }
 }
