b946d24d0f2e50180f53d10d26cdc701391cecb4
Moshe Dahan <38052895+dikodahan@users.noreply.github.com>
2023-09-20 15:37:55 -0400

    test updated channels table

--- frontend/src/channel-table.ts
+++ frontend/src/channel-table.ts
@@ -6,15 +6,17 @@ Vue.component("channel-table", {
         <table>
             <thead class="title-case">
                 <tr>
+                    <th>Logo</th>
                     <th>שם ערוץ</th>
                     <th v-for="(service, name) in services">{{ getServiceName(name) }}</th>
                 </tr>
             </thead>
             <tbody>
                 <tr v-for="name in channelNames">
+                    <td><img :src="getChannelLogo(name)" alt="Channel Logo" width="50"></td>
                     <td>{{ name }}</td>
                     <td v-for="(service, serviceName) in services">
-                    {{ service.includes(name) ? "✅" : "🛑" }}
+                        {{ service.includes(name) ? "✅" : "🛑" }}
                     </td>
                 </tr>
             </tbody>
@@ -25,19 +27,24 @@ Vue.component("channel-table", {
     return {
       services: {} as ChannelStats,
       comparison: [] as ComparisonService[],
+      lineup: [] as ChannelLineup[]
     };
   },
   async beforeMount() {
-    const [services, comparison] = await Promise.all([
+    const [services, comparison, lineup] = await Promise.all([
       fetch("/service-channel-names.json").then((res) =>
         res.json()
       ) as Promise<ChannelStats>,
       fetch("/comparison-services.json").then((res) => res.json()) as Promise<
         ComparisonService[]
       >,
+      fetch("/backend/src/services/channel-lineup.json").then((res) => res.json()) as Promise<
+        ChannelLineup[]
+      >,
     ]);
     this.services = services;
     this.comparison = comparison;
+    this.lineup = lineup;
   },
   methods: {
     getServiceName(service: string) {
@@ -45,12 +52,13 @@ Vue.component("channel-table", {
         this.comparison.find((s) => s.service === service)?.name ?? service
       );
     },
+    getChannelLogo(channelName: string) {
+        return this.lineup.find(channel => channel.name === channelName)?.logo;
+    }
   },
   computed: {
     channelNames() {
-      return Array.from(
-        new Set(Object.values(this.services).flatMap((channels) => channels))
-      );
+      return this.lineup.map(channel => channel.name);
     },
   },
 });
