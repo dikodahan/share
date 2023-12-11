d2c1591cc3ff3521107807cb6f08b3f0d0809f5c
Moshe Dahan <38052895+dikodahan@users.noreply.github.com>
2023-12-11 18:47:34 +0200

    Re-adding crystal service 2

--- backend/src/services/crystal/crystal.generator.ts
+++ backend/src/services/crystal/crystal.generator.ts
@@ -3,3 +3,24 @@ import channelLineup from "../channel-lineup.json";
 import { UserException } from "../../user-exception";
 import { epgGenerator } from "../epg.generator";
 
+export function* crystalGenerator(
+    username: string,
+    password: string
+  ): Generator<string, void, unknown> {
+    if (!username || !password || username == "USERNAME" || password == "PASSWORD") {
+      throw new UserException("Invalid username or password", 400);
+    }
+  
+    for (const line of epgGenerator()) {
+      yield line;
+    }
+  
+    for (const { channelName, channelId } of Crystal) {
+      const { extGrp, tvgId, tvgLogo } =
+        channelLineup[channelName as keyof typeof channelLineup];
+      yield "";
+      yield `#EXTINF:-1 tvg-id="${tvgId}" tvg-logo="${tvgLogo}",${channelName}`;
+      yield `#EXTGRP:${extGrp}`;
+      yield `http://crystal.ottc.pro:80/${username}/${password}/${channelId}`;
+    }
+  }
