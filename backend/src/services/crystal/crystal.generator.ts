59615a59241f6aa2b57803634d7a8c5d53f45684
Moshe Dahan <38052895+dikodahan@users.noreply.github.com>
2023-12-11 18:43:18 +0200

    Re-adding crystal service 1

---
+++ backend/src/services/crystal/crystal.generator.ts
@@ -0,0 +1,5 @@
+import Crystal from "./crystal.json";
+import channelLineup from "../channel-lineup.json";
+import { UserException } from "../../user-exception";
+import { epgGenerator } from "../epg.generator";
+
