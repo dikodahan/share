2a224823b603014cedefafb02ed2f851e2070ec3
Moshe Dahan <38052895+dikodahan@users.noreply.github.com>
2023-12-20 23:53:33 +0200

    Update AF DM as well

--- backend/src/services/antifrizdm/antifrizdm.generator.ts
+++ backend/src/services/antifrizdm/antifrizdm.generator.ts
@@ -1,4 +1,4 @@
-import AntiFrizDm from "./antifrizdm.json";
+import AntiFrizDm from "../antifriz/antifriz.json";
 import channelLineup from "../channel-lineup.json";
 import { UserException } from "../../user-exception";
 import { epgGenerator } from "../epg.generator";
@@ -19,11 +19,18 @@ export function* antiFrizDmGenerator(
   }
 
   for (const { channelName, channelId, tvgRec, catchupDays } of AntiFrizDm) {
-    const { extGrp, tvgId, tvgLogoDm } =
+    const { extGrp, tvgId, tvgLogoDm, source } =
       channelLineup[channelName as keyof typeof channelLineup];
-    yield "";
-    yield `#EXTINF:0 tvg-id="${tvgId}" tvg-logo="${tvgLogoDm}" catchup-source="${BASE_URL}/${channelId}/${CATCHUP_ENDPOINT}?token=${token}" tvg-rec="${tvgRec}" catchup-days="${catchupDays}",${channelName}`;
-    yield `#EXTGRP:${extGrp}`;
-    yield `${BASE_URL}:1600/s/${token}/${channelId}/video.m3u8`;
+    if (source != "origin") {
+      yield "";
+      yield `#EXTINF:0 tvg-id="${tvgId}" tvg-logo="${tvgLogoDm}",${channelName}`;
+      yield `#EXTGRP:${extGrp}`;
+      yield `${source}`;
+    } else {
+      yield "";
+      yield `#EXTINF:0 tvg-id="${tvgId}" tvg-logo="${tvgLogoDm}" catchup-source="${BASE_URL}/${channelId}/${CATCHUP_ENDPOINT}?token=${token}" tvg-rec="${tvgRec}" catchup-days="${catchupDays}",${channelName}`;
+      yield `#EXTGRP:${extGrp}`;
+      yield `${BASE_URL}:1600/s/${token}/${channelId}/video.m3u8`;
+    }
   }
 }
