2fd228dda5f3f7b12a370a076b0cfb61a08dfd11
Moshe Dahan <38052895+dikodahan@users.noreply.github.com>
2023-12-22 19:17:23 +0200

    fixes for free service generator

--- backend/src/services/free/free.generator.ts
+++ backend/src/services/free/free.generator.ts
@@ -3,19 +3,25 @@ import channelLineup from "../channel-lineup.json";
 import { UserException } from "../../user-exception";
 import { epgGenerator } from "../epg.generator";
 
-for (const line of epgGenerator()) {
-    yield line;
-}
+const BASE_GRP = "טלוויזיה";
 
-for (const { channelName } of Free) {
-    const { extGrp, tvgId, tvgLogo, link } =
-      channelLineup[channelName as keyof typeof channelLineup];
-    yield "";
-    yield `#EXTINF:0 tvg-id="${tvgId}" tvg-logo="${tvgLogo}",${channelName}`;
-    if (extGrp == "רדיו") {
-        yield `#EXTGRP:${extGrp}`;
-    } else {
-        yield `#EXTGRP:טלוויזיה`;
+export function* freeGenerator(
+  ): Generator<string, void, unknown> {
+
+    for (const line of epgGenerator()) {
+        yield line;
     }
-    yield `${link}`;
-}
+
+    for (const { channelName } of Free) {
+        const { extGrp, tvgId, tvgLogo, link } =
+        channelLineup[channelName as keyof typeof channelLineup];
+        yield "";
+        yield `#EXTINF:0 tvg-id="${tvgId}" tvg-logo="${tvgLogo}",${channelName}`;
+        if (extGrp == "רדיו") {
+            yield `#EXTGRP:${extGrp}`;
+        } else {
+            yield `#EXTGRP:${BASE_GRP}`;
+        }
+        yield `${link}`;
+    }
+  }
