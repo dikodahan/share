422e18ab3a201bab6e7714e52efcbc95dca47f49
Moshe Dahan <38052895+dikodahan@users.noreply.github.com>
2023-12-24 17:37:13 +0200

    testing channel lineup sorting

--- backend/src/services/test/test.generator.ts
+++ backend/src/services/test/test.generator.ts
@@ -18,19 +18,22 @@ export function* testGenerator(
     yield line;
   }
 
-  for (const { channelName, channelId, tvgRec, catchupDays } of Test) {
-    const { extGrp, tvgId, tvgLogo, link } =
-      channelLineup[channelName as keyof typeof channelLineup];
-    if (channelId == "none") {
-      yield "";
-      yield `#EXTINF:0 tvg-id="${tvgId}" tvg-logo="${tvgLogo}",${channelName}`;
-      yield `#EXTGRP:${extGrp}`;
-      yield `${link}`;
-    } else {
-      yield "";
-      yield `#EXTINF:0 tvg-id="${tvgId}" tvg-logo="${tvgLogo}" catchup-source="${BASE_URL}/${channelId}/${CATCHUP_ENDPOINT}?token=${token}" tvg-rec="${tvgRec}" catchup-days="${catchupDays}",${channelName}`;
-      yield `#EXTGRP:${extGrp}`;
-      yield `${BASE_URL}:1600/s/${token}/${channelId}/video.m3u8`;
+  // Create a set of available channel names from test.json for quick lookup
+  const availableChannels = new Set(Test.map(channel => channel.channelName));
+
+  // Iterate based on the order in channelLineup.json
+  for (const channelName of Object.keys(channelLineup)) {
+    // Check if the channel is in the test.json file
+    if (availableChannels.has(channelName)) {
+      const { tvgId, tvgLogo, link, extGrp } = channelLineup[channelName as keyof typeof channelLineup];
+      const testChannel = Test.find(c => c.channelName === channelName);
+
+      if (testChannel && testChannel.channelId !== "none") {
+        yield "";
+        yield `#EXTINF:0 tvg-id="${tvgId}" tvg-logo="${tvgLogo}" catchup-source="${BASE_URL}/${testChannel.channelId}/${CATCHUP_ENDPOINT}?token=${token}" tvg-rec="${testChannel.tvgRec}" catchup-days="${testChannel.catchupDays}",${channelName}`;
+        yield `#EXTGRP:${extGrp}`;
+        yield `${BASE_URL}:1600/s/${token}/${testChannel.channelId}/video.m3u8`;
+      }
     }
   }
 }
