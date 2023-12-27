18f036a3510f3ce61b6a650d73e21845711f829f
Moshe Dahan <38052895+dikodahan@users.noreply.github.com>
2023-12-27 03:26:39 +0200

    new channel type designation in channels table

--- frontend/src/channel-table.ts
+++ frontend/src/channel-table.ts
@@ -16,26 +16,26 @@ interface ChannelStats {
 
 Vue.component("channel-table", {
   template: `
-  <div class="fixTableHead">
-      <h1 class="hebh1"><u>טבלת השוואת ערוצי ישראל לכל ספק</u></h1>
-      <table>
-          <thead class="title-case">
-              <tr>
-                  <th>לוגו ערוץ</th> <!-- Changed from שם ערוץ to לוגו ערוץ -->
-                  <th v-for="(service, name) in services">{{ getServiceName(name) }}</th>
-              </tr>
-          </thead>
-          <tbody>
-              <tr v-for="channelName in channelNames">
-                  <td><img :src="getChannelLogo(channelName)" alt="Logo"></td> <!-- Use channel logos here -->
-                  <td v-for="(service, serviceName) in services">
-                  {{ hasChannel(service, channelName) ? "✅" : "🛑" }}
-                  </td>
-              </tr>
-          </tbody>  
-      </table>
-  </div>
-  `,
+    <div class="fixTableHead">
+        <h1 class="hebh1"><u>טבלת השוואת ערוצי ישראל לכל ספק</u></h1>
+        <table>
+            <thead class="title-case">
+                <tr>
+                    <th>לוגו ערוץ</th>
+                    <th v-for="(service, name) in services">{{ getServiceName(name) }}</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="channelName in channelNames">
+                    <td><img :src="getChannelLogo(channelName)" alt="Logo"></td>
+                    <td v-for="(service, serviceName) in services">
+                    {{ hasChannel(service, channelName) }}
+                    </td>
+                </tr>
+            </tbody>  
+        </table>
+    </div>
+    `,
   data() {
     return {
       services: {} as ChannelStats,
@@ -44,26 +44,28 @@ Vue.component("channel-table", {
     };
   },
   async beforeMount() {
-    const [servicesData, comparisonData, channelLineupData] = await Promise.all([
-      fetch("/service-channel-names.json").then((res) => res.json()),
-      fetch("/comparison-services.json").then((res) => res.json()),
-      fetch("/channel-lineup.json").then((res) => res.json())
+    const [services, comparison, channelLineup] = await Promise.all([
+      fetch("/service-channel-names.json").then((res) => res.json()) as Promise<ChannelStats>,
+      fetch("/comparison-services.json").then((res) => res.json()) as Promise<ComparisonService[]>,
+      fetch("/channel-lineup.json").then((res) => res.json()) as Promise<Record<string, any>>,
     ]);
-    this.services = servicesData as ChannelStats;
-    this.comparison = comparisonData as ComparisonService[];
-    this.channelLineup = channelLineupData as Record<string, any>;
+    this.services = services;
+    this.comparison = comparison;
+    this.channelLineup = channelLineup;
   },
   methods: {
     getServiceName(service: string) {
-      return (
-        this.comparison.find((s) => s.service === service)?.name ?? service
-      );
-    },
-    hasChannel(serviceChannels: ChannelInfo[], channelName: string) {
-      return serviceChannels.some((ci: ChannelInfo) => ci.channelName === channelName);
+      return this.comparison.find((s) => s.service === service)?.name ?? service;
     },
     getChannelLogo(channelName: string) {
       return this.channelLineup[channelName]?.tvgLogo || 'https://raw.githubusercontent.com/dikodahan/dikodahan.github.io/master/creative/img/Categories/DikoPlus-icon.png';
+    },
+    hasChannel(serviceChannels: ChannelInfo[], channelName: string) {
+      const channelInfo = serviceChannels.find((ci: ChannelInfo) => ci.channelName === channelName);
+      if (channelInfo) {
+        return channelInfo.channelId === 'none' || channelInfo.channelId === 1010 ? "❎" : "✅";
+      }
+      return "🛑";
     },
   },
   computed: {
