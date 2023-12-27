a6adc522d9b89baa45515f19f1ac4dd5851b8b2a
Moshe Dahan <38052895+dikodahan@users.noreply.github.com>
2023-12-27 22:48:14 +0200

    trying to fix channel build

--- backend/src/build-channels.ts
+++ backend/src/build-channels.ts
@@ -2,7 +2,6 @@ import * as fs from "fs";
 import * as path from "path";
 import ComparisonServices from "./comparison-services.json";
 import ChannelLineup from "./services/channel-lineup.json";
-import External from "./services/manual-services.json";
 
 
 export interface ChannelInfo {
