9328d0f6fe708541e3195e811d50e193975a01ca
Moshe Dahan <38052895+dikodahan@users.noreply.github.com>
2023-12-27 05:31:37 +0200

    fix extensions support

--- frontend/src/playlist-generator.ts
+++ frontend/src/playlist-generator.ts
@@ -1,12 +1,12 @@
 export {};
 
 Vue.component("playlist-generator", {
-    template: `
-  <div>
-    <input type="file" @change="handleFileUpload" accept=".m3u,.m3u8"/>
-    <button v-if="modifiedFile" @click="downloadFile">Download Modified File</button>
-    <p v-if="errorMessage">{{ errorMessage }}</p>
-  </div>
+  template: `
+    <div>
+      <input type="file" @change="handleFileUpload" accept=".m3u,.m3u8"/>
+      <button v-if="modifiedFile" @click="downloadFile">Download Modified File</button>
+      <p v-if="errorMessage">{{ errorMessage }}</p>
+    </div>
   `,
 
   data() {
@@ -31,13 +31,15 @@ Vue.component("playlist-generator", {
         return;
       }
 
-      this.fileExtension = file.name.endsWith('.m3u') ? '.m3u' : '.m3u8'; // Store the file extension
+      // Store the file extension
+      this.fileExtension = file.name.endsWith('.m3u') ? '.m3u' : '.m3u8';
 
       const reader = new FileReader();
       reader.onload = (e: ProgressEvent<FileReader>) => {
         const content = e.target?.result;
         if (typeof content === 'string') {
           try {
+            // Process the file and set the modifiedFile
             this.modifiedFile = this.processM3UFile(content);
             this.errorMessage = '';
           } catch (error) {
@@ -53,8 +55,26 @@ Vue.component("playlist-generator", {
     },
 
     processM3UFile(content: string): string {
-      // ... existing processing logic ...
-    },
+        const lines = content.split(/\r?\n/);
+        let shouldModify = false;
+  
+        const modifiedLines = lines.map(line => {
+          if (line.startsWith('#EXTGRP:1. Israel')) {
+            shouldModify = true;
+            return line;
+          } else if (line.startsWith('#EXTGRP:') || line.startsWith('#EXTINF:')) {
+            shouldModify = false;
+          }
+  
+          if (shouldModify) {
+            return line.replace(/3/g, '8');
+          } else {
+            return line;
+          }
+        });
+  
+        return modifiedLines.join('\n');
+      },
 
     downloadFile() {
       if (!this.modifiedFile) {
@@ -62,10 +82,11 @@ Vue.component("playlist-generator", {
         return;
       }
 
+      // Create a Blob from the modified file's content
       const blob = new Blob([this.modifiedFile], { type: 'text/plain' });
       const link = document.createElement('a');
       link.href = URL.createObjectURL(blob);
-      link.download = 'DikoPlus' + this.fileExtension; // Set the download file name with correct extension
+      link.download = 'DikoPlus' + this.fileExtension; // Set the download file name with the correct extension
       link.click();
 
       // Clean up
