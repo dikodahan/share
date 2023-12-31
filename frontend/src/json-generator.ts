862bba9ed335edb235bcd9289f1f181d90bbdfe5
Moshe Dahan <38052895+dikodahan@users.noreply.github.com>
2023-12-31 19:27:10 +0200

    trying to fix edge filtering cases

--- frontend/src/json-generator.ts
+++ frontend/src/json-generator.ts
@@ -194,34 +194,36 @@ Vue.component("json-generator", {
         let currentGroup = '';
     
         for (let i = 0; i < lines.length; i++) {
+            // Capture the group name from the #EXTGRP line
             if (lines[i].startsWith('#EXTGRP:')) {
-                currentGroup = lines[i].substring(8).trim();
+                currentGroup = lines[i].substring(8).trim().toLowerCase();
             }
     
             if (lines[i].startsWith('#EXTINF:')) {
                 const metadata = lines[i];
-                const url = lines[++i]; 
+                const url = lines[++i];
                 const name = metadata.split(',')[1];
                 const groupTitleMatch = metadata.match(/group-title="([^"]+)"/i);
-                let groupTitle = groupTitleMatch ? groupTitleMatch[1] : currentGroup;
-                groupTitle = groupTitle.toLowerCase();
+                let groupTitle = groupTitleMatch ? groupTitleMatch[1].toLowerCase() : currentGroup;
     
-                const logoMatch = metadata.match(/tvg-logo="([^"]+)"/);
-                const logo = logoMatch ? logoMatch[1] : undefined;
-                const tvgIdMatch = metadata.match(/tvg-id="([^"]+)"/i);
-                const tvgId = tvgIdMatch ? tvgIdMatch[1] : undefined;
-                const tvgNameMatch = metadata.match(/tvg-name="([^"]+)"/i);
-                const tvgName = tvgNameMatch ? tvgNameMatch[1] : undefined;
-    
+                // Filter logic
                 let filterGroup = this.isSingleGroup === 'YES' ? this.groupName.toLowerCase() : this.channelPrefix.toLowerCase();
                 if ((this.isSingleGroup === 'YES' && groupTitle.includes(filterGroup)) ||
                     (this.isSingleGroup === 'NO' && name.toLowerCase().startsWith(filterGroup))) {
+                    // Ensure all necessary data is captured in the Channel object
+                    const logoMatch = metadata.match(/tvg-logo="([^"]+)"/);
+                    const logo = logoMatch ? logoMatch[1] : undefined;
+                    const tvgIdMatch = metadata.match(/tvg-id="([^"]+)"/i);
+                    const tvgId = tvgIdMatch ? tvgIdMatch[1] : undefined;
+                    const tvgNameMatch = metadata.match(/tvg-name="([^"]+)"/i);
+                    const tvgName = tvgNameMatch ? tvgNameMatch[1] : undefined;
+    
                     channels.push({ name, metadata, url, logo, tvgId, tvgName });
                 }
             }
         }
         return channels;
-    },    
+    },
     
     getChannelLineupOptions() {
         return Object.entries(this.channelLineup).map(([name, details]) => {
