a2f73e2befcd4bf2670514b1ffc0ef785c5d1457
Moshe Dahan <38052895+dikodahan@users.noreply.github.com>
2024-01-05 01:28:04 +0200

    complete livego and antifriz switch

--- backend/src/services/antifrizdm/antifrizdm.generator.ts
+++ backend/src/services/antifrizdm/antifrizdm.generator.ts
@@ -2,11 +2,12 @@ import AntiFriz from "../antifriz/antifriz.json";
 import channelLineup from "../channel-lineup.json";
 import { UserException } from "../../user-exception";
 import { epgGenerator } from "../epg.generator";
+import Free from "../free/free.json";
 
 const BASE_URL = "http://bethoven.af-stream.com";
 const CATCHUP_ENDPOINT = "video-${start}-${duration}.m3u8";
 
-export function* antiFrizDmGenerator(
+export function* antiFrizGenerator(
   _: string,
   token: string
 ): Generator<string, void, unknown> {
@@ -18,37 +19,38 @@ export function* antiFrizDmGenerator(
     yield line;
   }
 
-  const antifrizChannels = new Map<string, Array<typeof AntiFriz[number]>>();
+  const antiFrizChannels = new Map<string, Array<typeof AntiFriz[number]>>();
   AntiFriz.forEach(channel => {
-    if (antifrizChannels.has(channel.channelName)) {
-      antifrizChannels.get(channel.channelName)?.push(channel);
+    if (antiFrizChannels.has(channel.channelName)) {
+      antiFrizChannels.get(channel.channelName)?.push(channel);
     } else {
-      antifrizChannels.set(channel.channelName, [channel]);
+      antiFrizChannels.set(channel.channelName, [channel]);
     }
   });
 
+  const freeChannelSet = new Set(Free.map(c => c.channelName));
+
   for (const channelName of Object.keys(channelLineup)) {
-    const antifrizChannelArray = antifrizChannels.get(channelName);
+    const antiFrizChannelArray = antiFrizChannels.get(channelName);
+    const channelData = channelLineup[channelName as keyof typeof channelLineup];
 
-    if (antifrizChannelArray) {
-      for (const antifrizChannel of antifrizChannelArray) {
-        const { channelId, tvgRec, catchupDays } = antifrizChannel;
-        const channelData = channelLineup[channelName as keyof typeof channelLineup];
+    if (antiFrizChannelArray) {
+      for (const antiFrizChannel of antiFrizChannelArray) {
+        const { channelId, tvgRec, catchupDays } = antiFrizChannel;
+        const { tvgId, tvgLogoDm, extGrp } = channelData;
 
-        const { tvgId, tvgLogoDm, link, extGrp } = channelData;
-
-        if (channelId == "none") {
-          yield "";
-          yield `#EXTINF:0 tvg-id="${tvgId}" tvg-logo="${tvgLogoDm}",${channelName}`;
-          yield `#EXTGRP:${extGrp}`;
-          yield `${link}`;
-        } else {
-          yield "";
-          yield `#EXTINF:0 tvg-id="${tvgId}" tvg-logo="${tvgLogoDm}" catchup-source="${BASE_URL}/${channelId}/${CATCHUP_ENDPOINT}?token=${token}" tvg-rec="${tvgRec}" catchup-days="${catchupDays}",${channelName}`;
-          yield `#EXTGRP:${extGrp}`;
-          yield `${BASE_URL}:1600/s/${token}/${channelId}/video.m3u8`;
-        }
+        yield "";
+        yield `#EXTINF:0 tvg-id="${tvgId}" tvg-logo="${tvgLogoDm}" catchup-source="${BASE_URL}/${channelId}/${CATCHUP_ENDPOINT}?token=${token}" tvg-rec="${tvgRec}" catchup-days="${catchupDays}",${channelName}`;
+        yield `#EXTGRP:${extGrp}`;
+        yield `${BASE_URL}:1600/s/${token}/${channelId}/video.m3u8`;
       }
-    }
+    } else if (freeChannelSet.has(channelName)) {
+      const { tvgId, tvgLogoDm, link, extGrp } = channelData;
+
+      yield "";
+      yield `#EXTINF:0 tvg-id="${tvgId}" tvg-logo="${tvgLogoDm}",${channelName}`;
+      yield `#EXTGRP:${extGrp}`;
+      yield `${link}`;
+  }
   }
 }
--- backend/src/services/livego/livego.generator.ts
+++ backend/src/services/livego/livego.generator.ts
@@ -2,8 +2,9 @@ import LiveGo from "./livego.json";
 import channelLineup from "../channel-lineup.json";
 import { UserException } from "../../user-exception";
 import { epgGenerator } from "../epg.generator";
+import Free from "../free/free.json";
 
-export function* liveGoGenerator(
+export function* testGenerator(
   username: string,
   password: string
 ): Generator<string, void, unknown> {
@@ -15,37 +16,38 @@ export function* liveGoGenerator(
     yield line;
   }
 
-  const livegoChannels = new Map<string, Array<typeof LiveGo[number]>>();
+  const liveGoChannels = new Map<string, Array<typeof LiveGo[number]>>();
   LiveGo.forEach(channel => {
-    if (livegoChannels.has(channel.channelName)) {
-      livegoChannels.get(channel.channelName)?.push(channel);
+    if (liveGoChannels.has(channel.channelName)) {
+      liveGoChannels.get(channel.channelName)?.push(channel);
     } else {
-      livegoChannels.set(channel.channelName, [channel]);
+      liveGoChannels.set(channel.channelName, [channel]);
     }
   });
 
+  const freeChannelSet = new Set(Free.map(c => c.channelName));
+
   for (const channelName of Object.keys(channelLineup)) {
-    const livegoChannelArray = livegoChannels.get(channelName);
+    const liveGoChannelArray = liveGoChannels.get(channelName);
+    const channelData = channelLineup[channelName as keyof typeof channelLineup];
 
-    if (livegoChannelArray) {
-      for (const livegoChannel of livegoChannelArray) {
-        const { tvgShift, tvgName, channelId } = livegoChannel;
-        const channelData = channelLineup[channelName as keyof typeof channelLineup];
+    if (liveGoChannelArray) {
+      for (const liveGoChannel of liveGoChannelArray) {
+        const { channelId, tvgShift, tvgName } = liveGoChannel;
+        const { tvgId, tvgLogo, extGrp } = channelData;
 
-        const { tvgId, tvgLogo, link, extGrp } = channelData;
-
-        if (channelId == 1010) {
-          yield "";
-          yield `#EXTINF:-1 tvg-id="${tvgId}" tvg-logo="${tvgLogo}",${channelName}`;
-          yield `#EXTGRP:${extGrp}`;
-          yield `${link}`;
-        } else {
-          yield "";
-          yield `#EXTINF:-1 tvg-id="${tvgId}" tvg-name="${tvgName}" tvg-shift="${tvgShift}" tvg-logo="${tvgLogo}",${channelName}`;
-          yield `#EXTGRP:${extGrp}`;
-          yield `http://livego.club:8080/${username}/${password}/${channelId}`;
-        }
+        yield "";
+        yield `#EXTINF:-1 tvg-id="${tvgId}" tvg-name="${tvgName}" tvg-shift="${tvgShift}" tvg-logo="${tvgLogo}",${channelName}`;
+        yield `#EXTGRP:${extGrp}`;
+        yield `http://livego.club:8080/${username}/${password}/${channelId}`;
       }
+    } else if (freeChannelSet.has(channelName)) {
+      const { tvgId, tvgLogo, link, extGrp } = channelData;
+
+      yield "";
+      yield `#EXTINF:-1 tvg-id="${tvgId}" tvg-logo="${tvgLogo}",${channelName}`;
+      yield `#EXTGRP:${extGrp}`;
+      yield `${link}`;
     }
   }
 }
