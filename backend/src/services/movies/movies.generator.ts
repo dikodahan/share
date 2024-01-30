2c03c6d47b74da9ff6ce4d45745e18b66b2d0d87
dikodahan <diko.dahan@gmail.com>
2024-01-30 18:18:49 -0500

    Added movies and sorting.

--- backend/src/services/movies/movies.generator.ts
+++ backend/src/services/movies/movies.generator.ts
@@ -1,14 +1,18 @@
 import Movies from "./movies.json";
 import { epgGenerator } from "../epg.generator";
 
-export function* moviesGenerator(
-  ): Generator<string, void, unknown> {
+export function* moviesGenerator(): Generator<string, void, unknown> {
+
+    // Sort movies by release year while preserving original order within the same year
+    const sortedMovies = Movies.slice().sort((a, b) => {
+        return parseInt(a.release) - parseInt(b.release) || Movies.indexOf(a) - Movies.indexOf(b);
+    });
 
     for (const line of epgGenerator()) {
         yield line;
     }
 
-    for (const movie of Movies) {
+    for (const movie of sortedMovies) {
         yield ""; // Empty line as per your request
         const movieName = movie.hasOwnProperty('subs-1') ? movie.name : `${movie.name} [ללא כתוביות]`;
         yield `#EXTINF:${movie.length} tvg-id="${movie.imdb}" tvg-logo="${movie.poster}",${movieName} (${movie.release})`;
@@ -18,4 +22,4 @@ export function* moviesGenerator(
         yield `#YEAR:${movie.release}`;
         yield `https://dikoplus.cyclic.app/vod/nachotoy?code=${movie.code}`;
     }
-  }
+}
