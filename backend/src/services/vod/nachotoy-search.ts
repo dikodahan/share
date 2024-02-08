77c7f25c797d3224f9b87c6e51812361c737acf1
dikodahan <diko.dahan@gmail.com>
2024-02-07 21:53:13 -0500

    Switch call to import

--- backend/src/services/vod/nachotoy-search.ts
+++ backend/src/services/vod/nachotoy-search.ts
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer');
+import puppeteer from 'puppeteer';
 
 export async function scrapeMovies(searchQuery: string) {
     const browser = await puppeteer.launch();
